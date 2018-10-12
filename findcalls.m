e1314dcde7d8872abc1ca6aab0bd24693f88646d
Unknown <maimon.rose@gmail.com>
2018-10-11 17:08:55 -0700

    add function to manually classify calls using mic and loggers
    Also fixed bug in findcalls that was causing calls that overlapped between files to be missed

--- findcalls.m
+++ findcalls.m
@@ -17,14 +17,19 @@ function findcalls(wd,fs,varargin)
 
 pnames = {'fileType', 'outputDir', 'debug','params','dataVarName','high_filter_cutoff','low_cenv_filter_cutoff','adaptiveThreshold','thresh'};
 dflts  = {'wav', fullfile(wd, 'Analyzed_auto'),false ,[],'recsGroup',2000,1000,false,0.75e-3};
-[fileType,outputDir,debugFlag,params,dataVarName,high_filter_cutoff,low_cenv_filter_cutoff,adaptiveThreshold,thresh] = internal.stats.parseArgs(pnames,dflts,varargin{:});
+[fileType,outputDir,debugFlag,call_cut_params,dataVarName,high_filter_cutoff,low_cenv_filter_cutoff,adaptiveThreshold,thresh] = internal.stats.parseArgs(pnames,dflts,varargin{:});
+
+allParams = struct('fileType',fileType,'outputDir',outputDir,'debugFlag',debugFlag,...
+    'dataVarName',dataVarName,'high_filter_cutoff',high_filter_cutoff,...
+    'low_cenv_filter_cutoff',low_cenv_filter_cutoff,'adaptiveThreshold',adaptiveThreshold);
+
 
 if adaptiveThreshold
     n_std_thresh = 10;
     thresh = [];
 end
 
-if isempty(params)
+if isempty(call_cut_params)
     % (t>=calllength && H>rmsthresh && powerRatio<powerRatioThresh && wEnt<wEntThresh)
     callLength = 0.01; % in s
     mergethresh = 5e-3; % in s
@@ -32,7 +37,7 @@ if isempty(params)
     powerRatioThresh = Inf;
     wEntThresh = Inf;
     
-    params = struct('thresh',thresh,'callLength',callLength,'rmsthresh',rmsthresh,'mergethresh',mergethresh,'powerRatioThresh',powerRatioThresh,'wEntThresh',wEntThresh,'fs',fs);
+    call_cut_params = struct('thresh',thresh,'callLength',callLength,'rmsthresh',rmsthresh,'mergethresh',mergethresh,'powerRatioThresh',powerRatioThresh,'wEntThresh',wEntThresh,'fs',fs);
 end
 
 rec_files = dir(fullfile(wd,['*.' fileType]));
@@ -42,41 +47,59 @@ if ~isfolder(outputDir)
 end
 n_files = length(rec_files);
 
+
+[high_b, high_a] = butter(2,2*high_filter_cutoff/fs,'high'); % high pass filter for raw data
+[low_b,low_a] = butter(2,2*low_cenv_filter_cutoff/fs); % low pass filter for envelope
+
 for fln = 1:n_files
     filename=rec_files(fln).name;
     disp(filename)
     disp(['Analyzing file: ' num2str(fln) ' of ' num2str(n_files)])
     disp('...')
+    % load data and calculate envelope
+    data_raw = load_audio_data(wd,filename,fileType,dataVarName);
+    senv = get_envelope(data_raw,high_b,high_a,low_b,low_a);
     
-    switch fileType
-        case 'wav'
-            data_raw = audioread(fullfile(wd, filename));
-        case 'mat'
-            data_raw = load(fullfile(wd, filename));
-            if isfield(data_raw,'analyzed')
-               if data_raw.analyzed
-                   continue
-               end
-            end
-            data_raw = data_raw.(dataVarName);
-    end
-    
-    [high_b, high_a] = butter(2,2*high_filter_cutoff/fs,'high');
-    data = filtfilt(high_b,high_a,data_raw);
-    if adaptiveThreshold
-        data_round10 = 10*floor(length(data)/10);
+    if adaptiveThreshold % if requested set threshold to the minimum standard deviation across 10 equal sized chunks across file
+        data_round10 = 10*floor(length(data_raw)/10);
         reshape_data_MA = reshape(data(1:data_round10),[],10);
         thresh = n_std_thresh*min(std(reshape_data_MA));
+        call_cut_params.thresh = thresh;
     end
-    hilbenv = abs(hilbert(data));			
-    [b,a] = butter(2,2*low_cenv_filter_cutoff/fs);			
-    senv = filtfilt(b,a,hilbenv);
-    allWins = calcWins(senv,params(1));
-    [isCall,wins] = findCall(allWins,data_raw,params(1));
     
-    if length(params)==2 && any(isCall)
-        allWins = calcWins(senv,params(2));
-        [isCall,wins] = findCall(allWins,data_raw,params(2));
+    [allWins,multi_file_wins] = calcWins(senv,call_cut_params(1)); % generate potential windows containing calls
+    multi_file_wins = multi_file_wins{1};
+    
+    if ~isempty(multi_file_wins) % check for calls overlapping between 2 .wav files
+        % get succeeding file's data
+        next_filename = rec_files(fln+1).name;
+        next_data_raw = load_audio_data(wd,next_filename,fileType,dataVarName);
+        next_senv = get_envelope(next_data_raw,high_b,high_a,low_b,low_a);
+        next_senv = [senv(multi_file_wins(1):end); next_senv];
+        % get end sample of call window in next file
+        [next_file_wins, next_file_multi_wins] = calcWins(next_senv,call_cut_params(1));
+        if ~isempty(next_file_multi_wins{2})
+            next_file_wins = vertcat(next_file_multi_wins{2},next_file_wins);
+        end
+        next_file_wins = merge_wins(next_file_wins,fs,mergethresh);
+        next_file_wins = next_file_wins(1,:) - diff(multi_file_wins)+1;
+        % concatenate data across files
+        multi_file_data_raw = [data_raw(multi_file_wins(1):end); next_data_raw(1:next_file_wins(2))];
+        multi_file_win = [1 length(multi_file_data_raw)];
+        isCall = findCall(multi_file_win,multi_file_data_raw,call_cut_params(1));
+        
+        if isCall
+            callpos = [multi_file_wins(1) next_file_wins(1,2)];
+            cut = multi_file_data_raw; 
+            save(fullfile(outputDir, [filename(1:end-4) '_' next_filename(1:end-4) '_Call_' sprintf('%03d',1) '.mat']),'cut','callpos','fs','allParams','call_cut_params');
+        end
+    end
+    
+    [isCall,wins] = findCall(allWins,data_raw,call_cut_params(1)); % merge windows and assess if they contain calls
+    
+    if length(call_cut_params)==2 && any(isCall) % do a second pass if requested
+        allWins = calcWins(senv,call_cut_params(2));
+        [isCall,wins] = findCall(allWins,data_raw,call_cut_params(2));
     end
     
     if debugFlag
@@ -98,10 +121,10 @@ for fln = 1:n_files
         end
     else
         file_callcount = 0;
-        for w = find(isCall)
+        for w = find(isCall) % save off calls and detection parameters
             callpos = wins(w,:);
             cut = data_raw(callpos(1):callpos(2));
-            save(fullfile(outputDir, [filename(1:end-4) '_Call_' sprintf('%03d',file_callcount) '.mat']),'cut','callpos','fs');
+            save(fullfile(outputDir, [filename(1:end-4) '_Call_' sprintf('%03d',file_callcount) '.mat']),'cut','callpos','fs','allParams','call_cut_params');
             file_callcount = file_callcount + 1;
         end
         if strcmp(fileType,'mat')
@@ -114,26 +137,58 @@ end
 
 end
 
-function wins = calcWins(senv,params)
+function senv = get_envelope(data_raw,high_b,high_a,low_b,low_a)
+
+data = filtfilt(high_b,high_a,data_raw);
+hilbenv = abs(hilbert(data));
+senv = filtfilt(low_b,low_a,hilbenv);
+
+end
+
+
+function data_raw = load_audio_data(wd,filename,fileType,dataVarName)
+
+switch fileType
+    case 'wav'
+        data_raw = audioread(fullfile(wd, filename));
+    case 'mat'
+        data_raw = load(fullfile(wd, filename));
+        if isfield(data_raw,'analyzed')
+            if data_raw.analyzed
+                data_raw = [];
+                return
+            end
+        end
+        data_raw = data_raw.(dataVarName);
+end
+
+end
+
+function [wins,multi_file_wins] = calcWins(senv,params)
 
 thresh = params.thresh;
 fs = params.fs;
 mergethresh = params.mergethresh;
+multi_file_wins = cell(1,2);
 
 if length(unique(sign(senv-thresh))) > 1
     thresh_up = find(diff(sign(senv-thresh))==2);
     thresh_down = find(diff(sign(senv-thresh))==-2);
-    if isempty(thresh_up) || isempty(thresh_down) % check that threhold crossing happens within file and not at beginning or end
+    if isempty(thresh_up) && isempty(thresh_down) 
         wins = [];
     else
-        if length(thresh_up)==length(thresh_down) && thresh_up(1)<thresh_down(1)
+        if length(thresh_up)==length(thresh_down) && thresh_up(1)<thresh_down(1) % (A) all windows contained in current .wav file
             wins = [thresh_up,thresh_down];
-        elseif length(thresh_up)>length(thresh_down)
+        elseif length(thresh_up)>length(thresh_down) % (B) one window starts in this file and continues until the end of the file
             wins = [thresh_up(1:end-1),thresh_down];
-        elseif length(thresh_down)>length(thresh_up)
+            multi_file_wins{1} = [thresh_up(end) length(senv)];
+        elseif length(thresh_down)>length(thresh_up) % (C) one window starts at the first sample of this file
             wins = [thresh_up,thresh_down(2:end)];
-        elseif length(thresh_up)==length(thresh_down) && thresh_up(1)>thresh_down(1)
+            multi_file_wins{2} = [1 thresh_down(1)]; 
+        elseif length(thresh_up)==length(thresh_down) && thresh_up(1)>thresh_down(1) % (B) AND (C)
             wins = [thresh_up(1:end-1),thresh_down(2:end)];
+            multi_file_wins{1} = [thresh_up(end) length(senv)];
+            multi_file_wins{2} = [1 thresh_down(1)];
         end
         if ~isempty(wins)
             wins = merge_wins(wins,fs,mergethresh);
